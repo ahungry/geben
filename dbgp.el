4a6c024ab2c10c88b8c122f5875aea9263c36cd2
fujinaka.tohru <fujinaka.tohru@5c19be94-a10c-11dd-a887-7ffe1a420f8d>
2009-01-10 08:01:52 +0000

    [FIX] Implemented `geben-quit-window' [FIX] Type mismatch error was occurred in breakpoint list mode       when any breakpoint deletion was executed.

--- dbgp.el
+++ dbgp.el
@@ -1,10 +1,10 @@
 ;;; dbgp.el --- DBGp protocol interface
-;; $Id: $
+;; $Id$
 ;; 
 ;; Filename: dbgp.el
 ;; Author: reedom <fujinaka.tohru@gmail.com>
 ;; Maintainer: reedom <fujinaka.tohru@gmail.com>
-;; Version: 0.20
+;; Version: 0.21
 ;; URL: http://code.google.com/p/geben-on-emacs/
 ;; Keywords: DBGp, debugger, PHP, Xdebug, Perl, Python, Ruby, Tcl, Komodo
 ;; Compatibility: Emacs 22.1
